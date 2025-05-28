cc12bd4be04df8c9b700157c2afa39cd2638150c
borisbugaev <zippityzop@protonmail.com>
2025-05-28 19:10:35 -0400

    bon

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
-	std::cout << "Hello, world!";
+	// accept bee parameters as argument
+
+	// call map function for b_set
+	// call map function for b_primary
+	// call map function for each word in dictionary set
+	// print each word which passes to file
+
+	// 
 	return 0;
 }
