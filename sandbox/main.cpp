a0b45620539d6ba8110473758da1aaf2ed8e3881
borisbugaev <zippityzop@protonmail.com>
2025-05-28 22:42:11 -0400

    hasta manana

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -5,10 +5,18 @@
 
 /*Program to solve "NYT Spelling Bee" word puzzle relatively fast*/
 
-uint32_t map_char(char wc)
+uint32_t map_key_char(const char kc, const uint32_t str_map)
 {
-	const int asc_offset{96};
-	return 2^(int(wc) - asc_offset);
+	const uint32_t asc_offset{96};
+	const uint32_t init_key_map{(int(kc) - asc_offset)};
+	const uint32_t shift_key_map{init_key_map << 26};
+	return shift_key_map | str_map;
+}
+
+uint32_t map_char(const char wc)
+{
+	const uint32_t asc_offset{96};
+	return (1 << (int(wc) - asc_offset));
 }
 
 uint32_t map_str(std::string_view str_v)
@@ -19,10 +27,18 @@ uint32_t map_str(std::string_view str_v)
 	}
 	else
 	{
-		uint32_t map_pref{map_char(str_v[1])};
+		const uint32_t map_pref{map_char(str_v[1])};
 		str_v.remove_prefix(1);
 		return map_pref | map_str(str_v);
 	}
+}
+
+uint32_t map_compare(const uint32_t key_map, const uint32_t candidate_map)
+{
+	const uint32_t key_index{key_map >> 26};
+	const uint32_t key_reference{(1 << key_index)};
+	const uint32_t has_key{(candidate_map & key_reference)/key_reference}; //effectively bool
+	// need correct comparator, or a sequence therein
 }
 
 int main()
